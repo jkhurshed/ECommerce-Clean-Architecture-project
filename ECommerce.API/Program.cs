85c0c91d825321b6620f2aaf46674e09b7f448b7
jkhurshed <jkhurshed002@gmail.com>
2026-04-02 13:11:34 +0500

    adding Microsoft.EntityFrameworkCore.Design to ECommerce.API

--- ECommerce.API/Program.cs
+++ ECommerce.API/Program.cs
@@ -2,9 +2,6 @@ using ECommerce.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
